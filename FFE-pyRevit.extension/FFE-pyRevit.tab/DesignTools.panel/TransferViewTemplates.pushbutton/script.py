deb61a174eca8739ca37dd9a2db91561b85f29af
Kyle Guggenheim <kylegug@gmail.com>
2025-12-16 12:01:06 -0500

    added logging

--- FFE-pyRevit.extension/FFE-pyRevit.tab/DesignTools.panel/TransferViewTemplates.pushbutton/script.py
+++ FFE-pyRevit.extension/FFE-pyRevit.tab/DesignTools.panel/TransferViewTemplates.pushbutton/script.py
@@ -18,6 +18,7 @@ How-to:
 _________________________________________________________________
 Last update:
 - [03.25.2025] - v1.0 RELEASE
+- [12.16.2025] - v1.1 Added logging
 _________________________________________________________________
 Author: Kyle Guggenheim"""
 
@@ -33,6 +34,7 @@ from Autodesk.Revit.DB import Transaction, FilteredElementCollector
 
 #____________________________________________________________________ IMPORTS (PYREVIT)
 from pyrevit import forms
+from pyrevit.script import output
 
 #____________________________________________________________________ IMPORTS (CUSTOM)
 from GUI.forms import my_WPF, ListItem
@@ -53,6 +55,12 @@ doc     = __revit__.ActiveUIDocument.Document
 uidoc   = __revit__.ActiveUIDocument
 app     = __revit__.Application
 PATH_SCRIPT = os.path.dirname(__file__)
+
+
+output_window = output.get_output()
+
+log_status = ""
+action = "Transfer View Templates"
 
 # GLOBAL VARIABLES
 dict_projects = {d.Title:d for d in app.Documents if not d.IsFamilyDocument and not d.IsLinked}
@@ -295,3 +303,77 @@ class CopyViewTemplate(my_WPF):
 #____________________________________________________________________ MAIN
 if __name__ == '__main__':
     CopyViewTemplate()
+
+
+log_status = "Success"
+#______________________________________________________ LOG ACTION
+# action = "Project Info Comparison"
+def log_action(action, log_status):
+    """Log action to user JSON log file."""
+    import os, json, time
+    from pyrevit import revit
+
+    doc = revit.doc
+    doc_path = doc.PathName or "<Untitled>"
+
+    doc_title = doc.Title
+    version_build = doc.Application.VersionBuild
+    version_number = doc.Application.VersionNumber
+    username = doc.Application.Username
+    action = action
+
+    # json log location
+    # \FFE Inc\FFE Revit Users - Documents\00-General\Revit_Add-Ins\FFE-pyRevit\Logs
+    log_dir = os.path.join(os.path.expanduser("~"), "FFE Inc", "FFE Revit Users - Documents", "00-General", "Revit_Add-Ins", "FFE-pyRevit", "Logs")
+    log_file = os.path.join(log_dir, username + "_revit_log.json")
+
+    dataEntry = {
+        "datetime": time.strftime("%Y-%m-%d %H:%M:%S"),
+        "username": username,
+        "doc_title": doc_title,
+        "doc_path": doc_path,
+        "revit_version_number": version_number,
+        "revit_build": version_build,
+        "action": action,
+        "status": log_status
+    }
+
+    # Function to write JSON data
+    def write_json(dataEntry, filename=log_file):
+        with open(filename,'r+') as file:
+            file_data = json.load(file)                 # First we load existing data into a dict.   
+            file_data['action'].append(dataEntry)       # Join new_data with file_data inside emp_details
+            file.seek(0)                                # Sets file's current position at offset.
+            json.dump(file_data, file, indent = 4)      # convert back to json.
+
+
+    # Check if log file exists, if not create it
+    logcheck = False
+    if not os.path.exists(log_file):
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+        with open(log_file, 'w') as file:    
+            file.write('{"action": []}')                # create json structure
+        
+        # output_window.print_md("### **Created log file:** `{}`".format(log_file))
+
+    # If it does exist, write to it
+    # Check if "action" key exists, if not create it
+    with open(log_file,'r+') as file:
+        file_data = json.load(file)
+        if 'action' not in file_data:
+            file_data['action'] = []
+            file.seek(0)
+            json.dump(file_data, file, indent = 4)
+
+    try:
+        write_json(dataEntry)
+        logcheck = True
+        # output_window.print_md("### **Logged sync to JSON:** `{}`".format(log_file))
+    except Exception as e:
+        logcheck = False
+
+    return dataEntry
+
+# log_action(action, log_status)
+output_window.print_md("Logging action: {}".format(log_action(action, log_status)))
