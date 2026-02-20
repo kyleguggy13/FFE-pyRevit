419933d30ded6d4fad9979afc314a66d4974882c
Kyle Guggenheim <kylegug@gmail.com>
2026-02-20 18:08:44 -0500

    Add logging functionality to Collect Family Parameters: log user actions and status to JSON file

--- FFE-pyRevit.extension/FFE-pyRevit.tab/BetaTools.panel/CollectFamilyParameters.pushbutton/script.py
+++ FFE-pyRevit.extension/FFE-pyRevit.tab/BetaTools.panel/CollectFamilyParameters.pushbutton/script.py
@@ -513,3 +513,77 @@ class FamiliesWindow(forms.WPFWindow):
 # Run
 xaml_path = os.path.join(os.path.dirname(__file__), "FamiliesWindow.xaml")
 FamiliesWindow(xaml_path).ShowDialog()
+
+
+
+log_status = "Success"
+#______________________________________________________ LOG ACTION
+def log_action(action, log_status):
+    """Log action to user JSON log file."""
+    import os, json, time
+    from pyrevit import revit
+
+    doc = revit.doc
+    # doc_path = doc.PathName or "<Untitled>"
+    doc_path = doc.PathName if doc.PathName else "<Untitled>"
+
+    doc_title = doc.Title
+    version_build = doc.Application.VersionBuild
+    version_number = doc.Application.VersionNumber
+    username = doc.Application.Username
+    action = action
+
+    # json log location
+    # \FFE Inc\FFE Revit Users - Documents\00-General\Revit_Add-Ins\FFE-pyRevit\Logs
+    log_dir = os.path.join(os.path.expanduser("~"), "FFE Inc", "FFE Revit Users - Documents", "00-General", "Revit_Add-Ins", "FFE-pyRevit", "Logs")
+    log_file = os.path.join(log_dir, username + "_revit_log.json")
+
+    dataEntry = {
+        "datetime": time.strftime("%Y-%m-%d %H:%M:%S"),
+        "username": username,
+        "doc_title": doc_title,
+        "doc_path": doc_path,
+        "revit_version_number": version_number,
+        "revit_build": version_build,
+        "action": action,
+        "status": log_status
+    }
+
+    # Function to write JSON data
+    def write_json(dataEntry, filename=log_file):
+        with open(filename,'r+') as file:
+            file_data = json.load(file)                 # First we load existing data into a dict.   
+            file_data['action'].append(dataEntry)       # Join new_data with file_data inside emp_details
+            file.seek(0)                                # Sets file's current position at offset.
+            json.dump(file_data, file, indent = 4)      # convert back to json.
+
+
+    # Check if log file exists, if not create it
+    logcheck = False
+    if not os.path.exists(log_file):
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+        with open(log_file, 'w') as file:    
+            file.write('{"action": []}')                # create json structure
+        
+        # output_window.print_md("### **Created log file:** `{}`".format(log_file))
+
+    # If it does exist, write to it
+    # Check if "action" key exists, if not create it
+    with open(log_file,'r+') as file:
+        file_data = json.load(file)
+        if 'action' not in file_data:
+            file_data['action'] = []
+            file.seek(0)
+            json.dump(file_data, file, indent = 4)
+
+    try:
+        write_json(dataEntry)
+        logcheck = True
+        # output_window.print_md("### **Logged sync to JSON:** `{}`".format(log_file))
+    except Exception as e:
+        logcheck = False
+
+    return dataEntry
+
+log_action(action, log_status)
